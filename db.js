80ed1c3845e13019f5faec3a58b2e4cbe10f7ec2
balveer <balveerbhatiakk@gmail.com>
2022-08-22 18:29:04 +0530

    Database solved

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
 const mysql = require("mysql");
 
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "treePlantation",
+// });
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "treePlantation",
+  host: "162.241.85.25",
+  user: "maymojes_meandmytree",
+  password: "MyTree123*",
 });
 
 module.exports = db;
