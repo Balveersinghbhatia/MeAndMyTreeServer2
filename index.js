762c31daa889af7f2edfbdc8078ff88edd7fdf31
balveer <balveerbhatiakk@gmail.com>
2022-08-22 18:39:53 +0530

    PORT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const db = require("./db");
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 // Database connection
 db.connect((err) => {
   if (err) {
