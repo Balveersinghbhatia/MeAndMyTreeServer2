c097d9bfb2cc346275ba5c7a1d47b6084dfc0f1f
balveer <balveerbhatiakk@gmail.com>
2022-08-25 14:20:30 +0530

    added cors

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const db = require("./db");
-const cors = require(cors);
 const port = process.env.PORT || 5000;
 
 let app = express();
-
-app.use(cors());
 
 app.use(express.json());
 app.get("/", function (req, res) {
