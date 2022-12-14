204d513f4ad43d6cd84812ff3beaad0b5dcb9d75
balveer <balveerbhatiakk@gmail.com>
2022-12-14 20:13:43 +0530

    removed cors

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const port = process.env.PORT || 5000;
 const app = express();
 const MySQLStore = require("express-mysql-session")(session);
 
-// app.use(cors());
-app.use(express.json());
+app.use(cors());
+// app.use(express.json());
 app.use(cookieParser());
 // app.use(express.urlencoded());
 // To save the session in database
